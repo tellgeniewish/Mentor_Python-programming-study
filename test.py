9d61b3da103a395b22797eafa793411dd811469b
tellgeniewish <jadastras2@gmail.com>
2025-04-09 03:08:56 +0900

    week02 추가

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
-a, b, c = map(int, input("세 정수를 입력하세요: ").split())
+n = int(input("입력: "))
 
-if a == 0 or b == 0 or c == 0:
-    print("0은 포함될 수 없습니다.")
-elif a % 2 == 0 and b % 2 == 0 and c % 2 == 0:
-    print("모두 짝수입니다.")
-elif a % 2 != 0 and b % 2 != 0 and c % 2 != 0:
-    print("모두 홀수입니다.")
-else:
-    print("홀수와 짝수가 섞여 있습니다.")
+# 상단 부분
+for i in range(n):
+    print("*" * (i + 1) + " " * (2 * (n - i - 1)) + "*" * (i + 1))
+
+# 하단 부분
+for i in range(1, n):
+    print("*" * (n - i) + " " * (2 * i) + "*" * (n - i))
